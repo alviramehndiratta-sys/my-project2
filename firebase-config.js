b0e80458831ca1b58d862f95fe7aea420cb26c46
Alvira Mehndiratta <alviramehndiratta@gmail.com>
2026-03-23 17:02:16 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDDa96drqrvpOFcy_TlYTlZuPInur2wSFk",
+  authDomain: "travel-tourism-spa.firebaseapp.com",
+  databaseURL: "https://travel-tourism-spa-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "travel-tourism-spa",
+  storageBucket: "travel-tourism-spa.firebasestorage.app",
+  messagingSenderId: "202854188086",
+  appId: "1:202854188086:web:0a0b862f63a71b23f0c497"
+};
+
+firebase.initializeApp(firebaseConfig);
